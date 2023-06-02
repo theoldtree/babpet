ea9c58bfd96bcb0aea4091c6356348282bd6fdce
theoldtree <yjeongmu@gmail.com>
2023-06-03 00:07:36 +0900

    hide key

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   // Firebase에서 제공하는 구성 정보를 입력합니다.
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Firebase 초기화
