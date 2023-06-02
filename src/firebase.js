197bbae981a95cf8db014421abacf536e69a9184
theoldtree <yjeongmu@gmail.com>
2023-06-02 15:43:00 +0900

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // Firebase에서 제공하는 구성 정보를 입력합니다.
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// Firestore 초기화
+const firestore = getFirestore(app);
+
+export { app, firestore };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { app, firestore } from "./firebase";
+console.log(app, firestore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
